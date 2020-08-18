2ebde143a363bee4d861d783eb993592438e2f29
llirik1337 <aleshinwork.1337@gmail.com>
2020-08-18 13:05:34 +0300

    fix bug

--- resources/js/components/Home/Checking.vue
+++ resources/js/components/Home/Checking.vue
@@ -4,8 +4,8 @@
             <a-checkbox v-model="pair">
                 Pair EAN and DM
             </a-checkbox>
-            <br/>
-            <br/>
+            <br />
+            <br />
             <a-input
                 placeholder="EAN13"
                 ref="ean"
@@ -14,10 +14,14 @@
                 @pressEnter="endInputEan"
                 :allowClear="true"
             >
-
                 <!-- <span slot="addonBefore" class="input-prefix">{{state}}</span> -->
             </a-input>
-            <a-alert v-show="eanError" type="error" message="Ean code wrong" banner />
+            <a-alert
+                v-show="eanError"
+                type="error"
+                message="Ean code wrong"
+                banner
+            />
             <br /><br />
             <a-input
                 placeholder="DM"
@@ -27,98 +31,113 @@
                 @pressEnter="endInputDM"
                 :allowClear="true"
             >
-
                 <!-- <span slot="addonBefore" class="input-prefix">{{state}}</span> -->
             </a-input>
-            <a-alert v-show="dmError" type="error" :message="dmErrorMsg" banner />
-            <a-alert v-show="dmSuccess" type="success" message="DM code was applied successfully" banner />
+            <a-alert
+                v-show="dmError"
+                type="error"
+                :message="dmErrorMsg"
+                banner
+            />
+            <a-alert
+                v-show="dmSuccess"
+                type="success"
+                message="DM code was applied successfully"
+                banner
+            />
             <br /><br />
         </a-col>
     </a-row>
 </template>
 
 <script>
-    import {mapActions} from "vuex";
+import { mapActions } from "vuex";
 
-    export default {
-        name: "Checking",
-        data: ()=>({
-            codedm: '',
-            codeean: '',
-            codeeanId: '',
-            eanError: false,
-            dmError: false,
-            dmSuccess: false,
-            dmErrorMsg:  'DM code wrong',
-            pair: true,
-        }),
-        mounted() {
-            this.focusEan();
+export default {
+    name: "Checking",
+    data: () => ({
+        codedm: "",
+        codeean: "",
+        codeeanId: "",
+        eanError: false,
+        dmError: false,
+        dmSuccess: false,
+        dmErrorMsg: "DM code wrong",
+        pair: true
+    }),
+    mounted() {
+        this.focusEan();
+    },
+    methods: {
+        ...mapActions([
+            "checkingDMCode",
+            "searchCodeEan13",
+            "searchCodeDMByCode",
+            "setStatus"
+        ]),
+        focusEan() {
+            // this.dmSuccess = false;
+            this.eanError = false;
+            this.clearEan();
+            this.$refs.ean.focus();
         },
-        methods: {
-            ...mapActions(['checkingDMCode', 'searchCodeEan13', 'searchCodeDMByCode', 'setStatus']),
-            focusEan() {
-                // this.dmSuccess = false;
-                this.eanError = false;
-                this.clearEan()
-                this.$refs.ean.focus();
-            },
-            focusDM() {
-                this.dmSuccess = false;
-                this.dmError = false;
-                this.clearDM()
-                this.$refs.dm.focus();
-            },
-            clearEan() {
-                this.codeean = ''
-            },
-            clearDM() {
-                this.codedm = ''
-            },
-            async endInputEan() {
-                try {
-                    const res = await this.searchCodeEan13(this.codeean);
-                    console.log(res);
-                    if(res===undefined) {
-                        this.eanError = true;
-                        return ;
-                    }
-                    this.codeeanId = res.id;
-                    this.focusDM();
-                } catch (e) {
+        focusDM() {
+            this.dmSuccess = false;
+            this.dmError = false;
+            this.clearDM();
+            this.$refs.dm.focus();
+        },
+        clearEan() {
+            this.codeean = "";
+        },
+        clearDM() {
+            this.codedm = "";
+        },
+        async endInputEan() {
+            try {
+                const res = await this.searchCodeEan13({ code: this.codeean });
+                console.log(res);
+                if (res === undefined) {
                     this.eanError = true;
+                    return;
                 }
-            },
-            async endInputDM() {
-                try {
-                    const res = await this.checkingDMCode({codeean_id:this.codeeanId, codedm: this.codedm})
-                    console.log(res);
-                    if(!res) {
-                        this.dmError = true;
-                        this.dmErrorMsg = 'DM code wrong';
-                        return;
-                    }
-                    console.log(res.status.name)
-                    if(res.status.name!=='Print') {
-                        this.dmError = true;
-                        this.dmErrorMsg = 'DM code was previously applied';
-                        return ;
-                    }
-                    await this.setStatus({codedm_id:res.id, status: 'Inflicted'});
-                    this.dmSuccess = true;
-                    if (this.pair)
-                        this.focusEan();
-                    else
-                        this.focusDM();
-                } catch (e) {
+                this.codeeanId = res.id;
+                this.focusDM();
+            } catch (e) {
+                this.eanError = true;
+            }
+        },
+        async endInputDM() {
+            try {
+                const res = await this.checkingDMCode({
+                    codeean_id: this.codeeanId,
+                    codedm: this.codedm
+                });
+                console.log(res);
+                if (!res) {
                     this.dmError = true;
+                    this.dmErrorMsg = "DM code wrong";
+                    return;
                 }
+                console.log(res.status.name);
+                if (res.status.name !== "Print") {
+                    this.dmError = true;
+                    this.dmErrorMsg = "DM code was previously applied";
+                    return;
+                }
+                await this.setStatus({
+                    codedm_id: res.id,
+                    status: "Inflicted"
+                });
+                this.dmSuccess = true;
+                if (this.pair) this.focusEan();
+                else this.focusDM();
+            } catch (e) {
+                this.dmError = true;
             }
         }
     }
-
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
